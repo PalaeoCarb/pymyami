4f9c8301729e797b006b3448f79a2945697939d0
Oscar Branson <oscarbranson@gmail.com>
2022-03-07 10:05:48 +0000

    re-order tests

--- test/test_MyAMI_crosscheck.py
+++ test/test_MyAMI_crosscheck.py
@@ -5,6 +5,28 @@ import pandas as pd
 
 import myami
 
+class MyAMI_V1_crosscheck(unittest.TestCase):
+
+    def test_Fcorr(self):
+        checkfile = os.path.join(os.path.dirname(__file__), 'data/MyAMI_V1_Fcorr_checkvals.csv')
+        print(checkfile)
+        check = pd.read_csv(checkfile, index_col=0)
+        check.columns = ['T', 'S', 'Ca', 'Mg', 'KspC', 'K1', 'K2', 'KW', 'KB', 'KspA', 'K0', 'KS']
+
+        new_Fcorr = myami.calc_Fcorr(Sal=check.S.values, TempC=check['T'].values, Ca=check.Ca.values, Mg=check.Mg.values)
+
+        Ks = 'K0', 'K1', 'K2', 'KW', 'KB', 'KspA', 'KspC', 'KS'
+
+        print('Comparing Ks to MyAMI_V1')
+        for k in Ks:
+            v1 = check[k]
+            new = new_Fcorr[k]
+
+            maxpercentdiff = 100 * np.max(np.abs((v1 - new) / v1))
+            
+            print(f'  {k}: {maxpercentdiff:.2f}%')
+            self.assertLess(maxpercentdiff, 0.4, msg=f'Maximum difference in {k} correction factor too large: {maxpercentdiff}%')
+
 # class MyAMI_V2_crosscheck(unittest.TestCase):
 #     """Compare Fcorr factors"""
 
@@ -44,28 +66,6 @@ import myami
 #         for k in Fcorr_calc:
 #             maxpcdiff = np.max(np.abs(100 * (Fcorr_approx[k] - Fcorr_calc[k]) / Fcorr_calc[k]))
 #             self.assertLess(maxpcdiff, 0.6, msg=f'Approximate {k} is greater than 0.2% from calculated {k}.')
-
-class MyAMI_V1_crosscheck(unittest.TestCase):
-
-    def test_Fcorr(self):
-        checkfile = os.path.join(os.path.dirname(__file__), 'data/MyAMI_V1_Fcorr_checkvals.csv')
-        print(checkfile)
-        check = pd.read_csv(checkfile, index_col=0)
-        check.columns = ['T', 'S', 'Ca', 'Mg', 'KspC', 'K1', 'K2', 'KW', 'KB', 'KspA', 'K0', 'KS']
-
-        new_Fcorr = myami.calc_Fcorr(Sal=check.S.values, TempC=check['T'].values, Ca=check.Ca.values, Mg=check.Mg.values)
-
-        Ks = 'K0', 'K1', 'K2', 'KW', 'KB', 'KspA', 'KspC', 'KS'
-
-        print('Comparing Ks to MyAMI_V1')
-        for k in Ks:
-            v1 = check[k]
-            new = new_Fcorr[k]
-
-            maxpercentdiff = 100 * np.max(np.abs((v1 - new) / v1))
-            
-            print(f'  {k}: {maxpercentdiff:.2f}%')
-            self.assertLess(maxpercentdiff, 0.4, msg=f'Maximum difference in {k} correction factor too large: {maxpercentdiff}%')
 
 
 if __name__ == "__main__":
