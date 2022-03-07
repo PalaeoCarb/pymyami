d64e4f561c588789aff656463331b47f7ddcbcf3
Oscar Branson <oscarbranson@gmail.com>
2022-03-07 17:10:05 +0000

    move setup options

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    packages=find_packages(),
-    package_data={'myami': ['parameters/*.json', 'parameters/*.csv', 'parameters/*.py']},
-    include_package_data=True,
-    zip_safe=True)
+setup(packages=find_packages())
