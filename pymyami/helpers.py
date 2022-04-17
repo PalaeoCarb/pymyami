04c5522acb546bf3dd58df902fe4a562eb8ab6d0
Oscar Branson <ob266@cam.ac.uk>
2022-04-17 10:54:41 +1000

    move seawater composition to table

--- pymyami/helpers.py
+++ pymyami/helpers.py
@@ -3,6 +3,7 @@ Helper functions used throughout MyAMI.
 """
 import json
 import numpy as np
+import pandas as pd
 import pkg_resources as pkgrs
 
 def MyAMI_parameter_file(fname=''):
@@ -90,24 +91,11 @@ def standard_seawater(S=35.):
         anions = [OH, Cl, B(OH)4, HCO3, HSO4, CO3, SO4] 
     """
 
-    cation_concs = np.array([
-        0.00000001,  # H ion; pH of about 8
-        0.4689674,  # Na Millero et al., 2008; Dickson OA-guide
-        0.0102077,  # K Millero et al., 2008; Dickson OA-guide
-        0.0528171,  # Mg Millero et al., 2008; Dickson OA-guide
-        0.0102821,  # Ca Millero et al., 2008; Dickson OA-guide
-        0.0000907  # Sr Millero et al., 2008; Dickson OA-guide
-    ]) * S / 35.
-
-    anion_concs = np.array([
-        0.0000010,  # OH ion; pH of about 8
-        0.5458696,  # Cl Millero et al., 2008; Dickson OA-guide
-        0.0001008,  # BOH4 Millero et al., 2008; Dickson OA-guide; pH of about 8 -- borate,
-        0.0017177,  # HCO3 Millero et al., 2008; Dickson OA-guide
-        0.0282352 * 1e-6,  # HSO4 Millero et al., 2008; Dickson OA-guide
-        0.0002390,  # CO3 Millero et al., 2008; Dickson OA-guide
-        0.0282352  # SO4 Millero et al., 2008; Dickson OA-guide
-    ]) * S / 35.
+    sw = pd.read_csv(MyAMI_parameter_file('standard_seawater.csv'), index_col=0, comment='#')
+    
+    print(sw)
+    cation_concs = sw.loc[['H', 'Na', 'K', 'Mg', 'Ca', 'Sr'], :].values.T * S / 35
+    anion_concs = sw.loc[['OH', 'Cl', 'BOH4', 'HCO3', 'HSO4', 'CO3', 'SO4'], :].values.T * S / 35
 
     return cation_concs, anion_concs
 
